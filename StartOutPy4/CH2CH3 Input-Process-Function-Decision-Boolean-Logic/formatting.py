ef1f34d3a6a93695a976d0662b23e4ebf6377f12
edwardigarashi <40938410+edwardigarashi@users.noreply.github.com>
2019-05-06 04:45:16 -0700

    q
    q

---
+++ StartOutPy4/CH2CH3 Input-Process-Function-Decision-Boolean-Logic/formatting.py
@@ -0,0 +1,14 @@
+# Program 2-19 This is program demonstrates how a floating-point
+# number is displayed with no formatting
+
+#amount_due = 5000.0
+#monthly_payment = amount_due / 12.0
+#print('The monthly payment is', monthly_payment)
+
+
+# Program 2-20 This program demonstrates how a floating-point
+# number can be formatted.
+amount_due = 5000.0
+monthly_payment = amount_due / 12
+print('The monthly payment is',
+    format(monthly_payment, '.2f'))
